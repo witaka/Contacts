6857f1a627fd77b8a33bb145e186cacccc0d1b64
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2019-09-19 10:48:21 -0700

    remove column phone from Contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_205338) do
+ActiveRecord::Schema.define(version: 2019_09_19_173703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2019_09_18_205338) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address_line_1"
