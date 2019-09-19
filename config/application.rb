b45c141d98d3faae881d4be2852c2026a658174c
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2019-09-19 12:05:03 -0700

    Add gem rack-cors for handling CORS  Midify aplication.rb add port 3434 to rack-cors config.middleware  Add gem active_model_serializers

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,23 @@ module Contacts
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3434'
+        resource '/*', credentials: true, headers: :any, methods: [
+          :get, :post, :options, :delete, :patch, :put
+        ]
+      end
+      allow do
+       origins 'localhost:3435'
+       resource '/*', credentials: true, headers: :any, methods: [
+         :get, :post, :options, :delete, :patch, :put
+       ]
+     end
+    end
   end
 end
+
+
+
